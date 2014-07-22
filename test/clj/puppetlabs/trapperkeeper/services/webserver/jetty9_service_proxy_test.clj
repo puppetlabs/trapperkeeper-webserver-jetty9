6ce35ebedd5e6bda316fa1445502e55cc48f8c76
Preben Ingvaldsen <preben@puppetlabs.com>
2014-07-22 13:16:42 -0700

    (TK-31) Add proxy explosion test
    Add test that ensures that a proxy request explodes when given
    an extremely large cookie and the request buffer size is not
    configured.

--- test/clj/puppetlabs/trapperkeeper/services/webserver/jetty9_service_proxy_test.clj
+++ test/clj/puppetlabs/trapperkeeper/services/webserver/jetty9_service_proxy_test.clj
@@ -1,4 +1,5 @@
 (ns puppetlabs.trapperkeeper.services.webserver.jetty9-service-proxy-test
+  (:import (org.httpkit.client TimeoutException))
   (:require [clojure.test :refer :all]
             [puppetlabs.trapperkeeper.services.webserver.jetty9-service :refer :all]
             [puppetlabs.trapperkeeper.testutils.webserver.common :refer :all]
@@ -91,6 +92,24 @@
         (let [response (http-get "http://localhost:10000/hello-proxy/world")]
           (is (= (:status response) 200))
           (is (= (:body response) "Hello, World!")))))
+
+    (testing (str "proxy explodes on a large cookie if larger request-buffer-size"
+                  "is not specified")
+      (with-target-and-proxy-servers
+        {:target       {:host "0.0.0.0"
+                        :port 9000}
+         :proxy        {:host "0.0.0.0"
+                        :port 10000}
+         :proxy-config {:host "localhost"
+                        :port 9000
+                        :path "/hello"}}
+        (let [response (http-get "http://localhost:9000/hello/world")]
+          (is (= (:status response) 200))
+          (is (= (:body response) "Hello, World!")))
+        (is (thrown? TimeoutException (http-get "http://localhost:10000/hello-proxy/world"
+                                                {:headers {"Cookie" absurdly-large-cookie}
+                                                 :as      :text
+                                                 :timeout 3000})))))
 
     (testing "proxy does not explode on a large cookie when properly configured"
       (with-target-and-proxy-servers
