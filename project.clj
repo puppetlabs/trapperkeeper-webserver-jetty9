50d491ba7b85f25745285ef5cef70cb05db745d1
Jenkins CI <ci@puppetlabs.com>
2014-10-16 16:57:06 -0700

    lein-release plugin: preparing 0.9.0 release

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def tk-version "0.5.1")
 (def ks-version "0.7.2")
 
-(defproject puppetlabs/trapperkeeper-webserver-jetty9 "0.9.0-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-webserver-jetty9 "0.9.0"
   :description "We are trapperkeeper.  We are one."
   ;; Abort when version ranges or version conflicts are detected in
   ;; dependencies. Also supports :warn to simply emit warnings.
