bee8cda73878d99a0c3d0d7a148845bb9bd4cdce
Jeremy Barlow <jeremy.barlow@puppet.com>
2017-07-12 00:39:07 -0700

    (TK-149) Reload the crl file in the running webservers on change
    This commit uses the tk-filesystem-watcher service to register a change
    notification for the CRL file given to the Jetty webserver.  On
    detection of a change, the Jetty SSLContextFactory is reloaded.  This
    allows for the updated CRL file to be used without requiring a full
    webserver restart.  Runtime CRL updating now occurs when tk-jetty9 is
    used in a webservice stack which includes the optional
    tk-filesystem-watcher dependency.

---
+++ java/com/puppetlabs/trapperkeeper/services/webserver/jetty9/utils/InternalSslContextFactory.java
@@ -0,0 +1,166 @@
+package com.puppetlabs.trapperkeeper.services.webserver.jetty9.utils;
+
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+import org.eclipse.jetty.util.security.CertificateUtils;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.security.cert.CRL;
+import java.util.Collection;
+
+public class InternalSslContextFactory extends SslContextFactory {
+
+    private static int maxTries = 5;
+    private static int sleepInMillisecondsBetweenTries = 1000;
+    private static final Logger LOG =
+            Log.getLogger(InternalSslContextFactory.class);
+
+    private Collection<? extends CRL> _crls;
+
+    @Override
+    protected void checkNotStarted() {
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        synchronized (this) {
+            load();
+        }
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        synchronized (this) {
+            unload();
+        }
+        super.doStop();
+    }
+
+    @Override
+    protected Collection<? extends CRL> loadCRL(String crlPath) throws Exception {
+        Collection<? extends CRL> crls;
+
+        synchronized (this) {
+            if (_crls == null) {
+                crls = super.loadCRL(crlPath);
+            } else {
+                crls = _crls;
+            }
+        }
+
+        return crls;
+    }
+
+    private void load() throws Exception {
+        synchronized (this) {
+            super.doStart();
+        }
+    }
+
+    private void unload() throws Exception {
+        synchronized (this) {
+            super.doStop();
+        }
+    }
+
+    @Override
+    public SSLContext getSslContext() {
+        synchronized (this) {
+            return super.getSslContext();
+        }
+    }
+
+    @Override
+    public SSLServerSocket newSslServerSocket(String host,
+                                              int port,
+                                              int backlog)
+            throws IOException {
+        synchronized (this) {
+            return super.newSslServerSocket(host, port, backlog);
+        }
+    }
+
+    @Override
+    public SSLSocket newSslSocket() throws IOException {
+        synchronized (this) {
+            return super.newSslSocket();
+        }
+    }
+
+    @Override
+    public SSLEngine newSSLEngine() {
+        synchronized (this) {
+            return super.newSSLEngine();
+        }
+    }
+
+    @Override
+    public SSLEngine newSSLEngine(String host, int port) {
+        synchronized (this) {
+            return super.newSSLEngine(host, port);
+        }
+    }
+
+    @Override
+    public SSLEngine newSSLEngine(InetSocketAddress address) {
+        synchronized (this) {
+            return super.newSSLEngine(address);
+        }
+    }
+
+    public void reload() throws Exception {
+        synchronized (this) {
+            Exception reloadEx;
+            int tries = maxTries;
+            String crlPath = getCrlPath();
+            File crlPathAsFile = null;
+            long crlLastModified = 0;
+
+            if (crlPath != null) {
+                crlPathAsFile = new File(crlPath);
+                crlLastModified = crlPathAsFile.lastModified();
+            }
+
+            // Try to parse CRLs from the crlPath until it is successful
+            // or a hard-coded number of failed attempts have been made.
+            do {
+                reloadEx = null;
+                try {
+                    _crls = CertificateUtils.loadCRL(crlPath);
+                } catch (Exception e) {
+                    reloadEx = e;
+
+                    // If the CRL file has been updated since the last reload
+                    // attempt, reset the retry counter.
+                    if (crlPathAsFile != null &&
+                            crlLastModified != crlPathAsFile.lastModified()) {
+                        crlLastModified = crlPathAsFile.lastModified();
+                        tries = maxTries;
+                    } else {
+                        tries--;
+                    }
+
+                    if (tries == 0) {
+                        LOG.warn("Failed ssl context reload after " +
+                                maxTries + " tries.  CRL file is: " +
+                                crlPath, reloadEx);
+                    } else {
+                        Thread.sleep(sleepInMillisecondsBetweenTries);
+                    }
+                }
+            } while (reloadEx != null && tries > 0);
+
+            if (reloadEx == null) {
+                unload();
+                load();
+            }
+        }
+    }
+}
