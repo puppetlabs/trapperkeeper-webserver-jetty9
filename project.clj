bee280f4934baea85cb221157567ffe6e15f9498
vemv <vemv@users.noreply.github.com>
2020-09-23 20:02:22 +0200

    Release under threatgrid

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def jetty-version "9.4.28.v20200408")
 
-(defproject puppetlabs/trapperkeeper-webserver-jetty9 "4.1.1-SNAPSHOT"
+(defproject threatgrid/trapperkeeper-webserver-jetty9 "4.2.0"
   :description "A jetty9-based webserver implementation for use with the puppetlabs/trapperkeeper service framework."
   :url "https://github.com/puppetlabs/trapperkeeper-webserver-jetty9"
   :license {:name "Apache License, Version 2.0"
