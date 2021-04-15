c0e23316d85f07e9c3975d5b44fef1f0b2f1d316
Jenkins CI <ci@puppet.com>
2021-04-15 21:34:40 +0000

    Version 4.1.5

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def jetty-version "9.4.39.v20210325")
 
-(defproject puppetlabs/trapperkeeper-webserver-jetty9 "4.1.5-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-webserver-jetty9 "4.1.5"
   :description "A jetty9-based webserver implementation for use with the puppetlabs/trapperkeeper service framework."
   :url "https://github.com/puppetlabs/trapperkeeper-webserver-jetty9"
   :license {:name "Apache License, Version 2.0"
